88c760d1e0f683e5124c51dc6bab5b1f9bb71087
Ena Kovac <ehk75@cornell.edu>
2024-03-25 10:54:57 -0400

    Ena Kovac (ehk75) Added initial functionality to main
    - Ask user if they want to start the game
    -Added .ocamlformat

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+let rec game_start () =
+  let () = print_string "Would you like to enter Pet Simulator? Y/N: " in
+  match read_line () with
+  | "Y" -> print_endline "Welcome to Pet Simulator!"
+  | "N" -> print_endline "Please play the game!"; game_start ()
+  | _ -> print_endline "Please enter Y or N."; game_start ()
+
+let game_output () = 
+  game_start()
+
+let () = game_output ()
