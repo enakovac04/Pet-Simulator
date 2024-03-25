9ad66c40f2a55365ee5a4ccbf7fd1a68bd27c8c1
Ena Kovac <ehk75@cornell.edu>
2024-03-25 14:59:31 -0400

    Ena Kovac (ehk75) Used new functions in command line
    - Choose the type of pet
    - Choose the name
    - Prints the pet

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,5 @@
+open Cs_final
+
 let rec game_start () =
   let () = print_string "Would you like to enter Pet Simulator? Y/N: " in
   match read_line () with
@@ -9,20 +11,30 @@ let rec game_start () =
       print_endline "Please enter Y or N.";
       game_start ()
 
+let create_dog () = 
+  let () = print_string "What would you like to name your Dog: " in
+  let name_of_dog = read_line () in
+  Pet.create name_of_dog "Dog"
+
+let create_camel () =
+  let () = print_string "What would you like to name your Camel: " in
+  let name_of_camel = read_line () in
+  Pet.create name_of_camel "Camel"
+
 let rec game_select_animal () =
   let () =
-    print_endline "Which animal would you like to select?\nDog  Cat  Camel"
+    print_endline "Which animal would you like to select?\nDog  Camel"
   in
   match read_line () with
-  | "Dog" -> print_endline "Dog"
-  | "Cat" -> print_endline "Cat"
-  | "Camel" -> print_endline "Camel"
+  | "Dog" -> create_dog ()
+  | "Camel" -> create_camel ()
   | _ ->
       print_endline "Please choose one of the options";
       game_select_animal ()
 
 let game_output () =
   game_start ();
-  game_select_animal ()
+  let animal = game_select_animal () in
+  print_endline (Pet.to_string animal)
 
 let () = game_output ()
