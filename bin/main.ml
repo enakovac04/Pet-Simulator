2cac7a7b7e6d0cebf2c79826a89629f3ede53b7f
Ena Kovac <ehk75@cornell.edu>
2024-03-25 11:02:32 -0400

    README commit

--- bin/main.ml
+++ bin/main.ml
@@ -5,7 +5,16 @@ let rec game_start () =
   | "N" -> print_endline "Please play the game!"; game_start ()
   | _ -> print_endline "Please enter Y or N."; game_start ()
 
+let rec game_select_animal () = 
+  let () = print_endline "Which animal would you like to select?\nDog  Cat  Camel" in
+  match read_line () with 
+  | "Dog" -> print_endline "Dog"
+  | "Cat" -> print_endline "Cat"
+  | "Camel" -> print_endline "Camel"
+  | _ -> print_endline "Please choose one of the options"; game_select_animal ()
+
 let game_output () = 
-  game_start()
+  game_start();
+  game_select_animal ()
 
 let () = game_output ()
